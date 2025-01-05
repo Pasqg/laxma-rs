021e6ae64c149d322ad34dc87f34e6669af5cdff
Pasqg <pasquale.giovenale@gmail.com>
2025-01-05 09:25:14 +0100

    Add match_none and match_any combinators

--- src/parser/combinators.rs
+++ src/parser/combinators.rs
@@ -62,21 +62,57 @@ where
     }
 }
 
-fn regex<RuleId>(pattern: &str) -> impl Combinator<RuleId, &str>
+pub fn regex<RuleId>(pattern: &str) -> impl Combinator<RuleId, &str>
 where
     RuleId: Copy,
 {
     match_regex(None, pattern)
 }
 
+/*
+    Matches nothing. Used to make it easier to match both empty and non-empty sequences with one rule.
+*/
+pub fn match_none<RuleId, TokenType>() -> impl Combinator<RuleId, TokenType>
+where
+    RuleId: Copy,
+    TokenType: Copy + Eq,
+{
+    move |tokens: &TokenStream<TokenType>| ParserResult::succeeded(AST::empty(), tokens.clone())
+}
+
+/*
+    Matches anything, as long as the current and following tokens to not match the 'excluded' combinator (if provided).
+*/
+pub fn match_any<RuleId, TokenType>(
+    id: Option<RuleId>,
+    excluded: Option<impl Combinator<RuleId, TokenType>>,
+) -> impl Combinator<RuleId, TokenType>
+where
+    RuleId: Copy,
+    TokenType: Copy + Eq,
+{
+    move |tokens: &TokenStream<TokenType>| {
+        if tokens.not_done() {
+            if excluded.is_some() && excluded.as_ref().unwrap()(tokens).is_ok() {
+                return ParserResult::failed(tokens.clone());
+            }
+
+            let (token, remaining) = tokens.advance();
+            return ParserResult::succeeded(AST::new(id, vec![token], Vec::new()), remaining);
+        }
+        ParserResult::failed(tokens.clone())
+    }
+}
+
 #[cfg(test)]
 mod test {
     use std::rc::Rc;
 
-    use regex::Regex;
-
     use crate::parser::{
-        ast::AST, combinators::match_regex, parser_result::ParserResult, token_stream::TokenStream,
+        ast::AST,
+        combinators::{lit, match_any, match_none, match_regex},
+        parser_result::ParserResult,
+        token_stream::TokenStream,
     };
 
     use super::match_token;
@@ -147,6 +183,48 @@ mod test {
         let parser = match_regex(rule, r"[0-9]+");
 
         let result = parser(&tokens);
+        assert_eq!(result, ParserResult::new(false, AST::empty(), tokens));
+    }
+
+    #[test]
+    fn test_match_none_empty_stream() {
+        let tokens: TokenStream<&str> = TokenStream::new(Rc::new(Vec::new()));
+
+        let parser = match_none();
+
+        let result: ParserResult<&str, &str> = parser(&tokens);
+        assert_eq!(result, ParserResult::new(true, AST::empty(), tokens));
+    }
+
+    #[test]
+    fn test_match_none() {
+        let tokens: TokenStream<&str> = TokenStream::new(Rc::new(vec!["a"]));
+
+        let parser = match_none();
+
+        let result: ParserResult<&str, &str> = parser(&tokens);
+        assert_eq!(result, ParserResult::new(true, AST::empty(), tokens));
+    }
+
+    #[test]
+    fn test_match_any_success() {
+        let tokens: TokenStream<&str> = TokenStream::new(Rc::new(vec!["a"]));
+
+        let rule = Some("test");
+        let parser = match_any(rule, Some(lit("b")));
+
+        let result: ParserResult<&str, &str> = parser(&tokens);
+        assert_eq!(result, ParserResult::new(true, AST::new(rule, vec!["a"], Vec::new()), tokens.advance().1));
+    }
+
+    #[test]
+    fn test_match_any_failure() {
+        let tokens: TokenStream<&str> = TokenStream::new(Rc::new(vec!["b"]));
+
+        let rule = Some("test");
+        let parser = match_any(rule, Some(lit("b")));
+
+        let result: ParserResult<&str, &str> = parser(&tokens);
         assert_eq!(result, ParserResult::new(false, AST::empty(), tokens));
     }
 }
