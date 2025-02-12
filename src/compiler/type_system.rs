81c000dbbf059cd881e871f7197d9394e623ac08
Pasqg <pasquale.giovenale@gmail.com>
2025-02-12 11:44:33 +0100

    Add support for types of destructuring identifier bindings

--- src/compiler/type_system.rs
+++ src/compiler/type_system.rs
@@ -1,6 +1,9 @@
 use std::collections::{HashMap, HashSet};
 
-use super::{internal_repr::{Expression, FunctionDefinition, Program, Type}, TypeDefinition};
+use super::{
+    internal_repr::{Expression, FunctionDefinition, Program, Type},
+    DestructuringComponent, TypeDefinition,
+};
 
 pub(super) struct TypeInfo {
     pub(super) primitive_types: HashSet<String>,
@@ -29,7 +32,7 @@ impl TypeInfo {
                 ("-".to_string(), int_type.clone()),
                 ("*".to_string(), int_type.clone()),
                 ("/".to_string(), int_type.clone()),
-                ]),
+            ]),
             constant_types: HashMap::from([
                 ("true".to_string(), bool_type.clone()),
                 ("false".to_string(), bool_type.clone()),
@@ -38,7 +41,8 @@ impl TypeInfo {
     }
 
     pub fn add_user_type(&mut self, type_name: &String, type_definition: &TypeDefinition) {
-        self.user_types.insert(type_name.clone(), type_definition.def.to_owned());
+        self.user_types
+            .insert(type_name.clone(), type_definition.def.to_owned());
     }
 }
 
@@ -83,7 +87,7 @@ fn infer_expression_type(
                 if builtin.is_some() {
                     return Ok(builtin.unwrap().to_owned());
                 }
-                
+
                 Err(format!("Function '{}' was not defined", function_call.name))
             }
         }
@@ -93,7 +97,7 @@ fn infer_expression_type(
                 Ok(var_type.unwrap().to_owned())
             } else {
                 Err(format!(
-                    "Undefined identifier {var} in function {}. Known {:?}",
+                    "Undefined identifier '{var}' in function '{}'. Known {:?}",
                     current_function.name, identifier_types,
                 ))
             }
@@ -116,7 +120,9 @@ pub fn infer_function_type(
             Type::TypeParameter(_) => {}
             Type::SimpleType(_) | Type::ParametrizedType(_, _) => {
                 let arg_type = argument.typing.name();
-                if !type_info.primitive_types.contains(arg_type) && !program.types.contains_key(arg_type) {
+                if !type_info.primitive_types.contains(arg_type)
+                    && !program.types.contains_key(arg_type)
+                {
                     return Err(format!("'{}' is not a valid type", arg_type));
                 }
             }
@@ -130,7 +136,7 @@ pub fn infer_function_type(
     }
 
     let function_name = &current_function.name;
-    if current_function.bodies.len() == 1 {
+    if current_function.is_not_pattern_matched() {
         let result = infer_expression_type(
             program,
             type_info,
@@ -151,13 +157,65 @@ pub fn infer_function_type(
     } else {
         let mut branch_types = HashSet::new();
         for (destructuring, expression) in &current_function.bodies {
-            //todo: get additional identifier types from destructuring
-            //let identifier_types = HashMap::new();
+            let mut identifier_types = HashMap::new();
+            for (k, v) in &arg_types {
+                identifier_types.insert(k.clone(), v.clone());
+            }
+
+            let mut i = 0;
+            for component in &destructuring.components {
+                let arg = &current_function.arguments[i];
+                match component {
+                    DestructuringComponent::Identifier(identifier) if identifier.as_str() != "_" => {
+                        identifier_types.insert(
+                            identifier.clone(),
+                            arg_types.get(&arg.identifier).unwrap().clone(),
+                        );
+                    }
+                    //todo: don't do this twice in repl maybe?
+                    DestructuringComponent::Destructuring(destructuring) => {
+                        let result = program.types.get(arg.typing.name());
+                        if result.is_none() {
+                            return Err(format!("Cannot find type '{}' for argument '{}' of function '{function_name}'", arg.typing.name(), arg.identifier));
+                        }
+                        let variant = result.unwrap().variants.get(&destructuring.0);
+                        if variant.is_none() {
+                            return Err(format!("Cannot find variant '{}' for type '{}' in function '{function_name}'", destructuring.0, arg.typing.name()));
+                        }
+                        let variant = variant.unwrap();
+                        match variant {
+                            super::TypeVariant::Constant(name) => {
+                                if destructuring.1.len() != 0 {
+                                    return Err(format!("Variant '{name}' for type '{}' expects 0 components but got {} in function '{function_name}'", arg.typing.name(), destructuring.1.len()));
+                                }
+                            }
+                            super::TypeVariant::Cartesian(name, items) => {
+                                if items.len() != destructuring.1.len() {
+                                    return Err(format!("Variant '{name}' for type '{}' expects {} components but got {} in function '{function_name}'", arg.typing.name(), items.len(), destructuring.1.len()));
+                                }
+                                let mut k = 0;
+                                for inner_component in &destructuring.1 {
+                                    match inner_component {
+                                        DestructuringComponent::Identifier(identifier) if identifier.as_str() != "_" => {
+                                            println!("Added {}", identifier);
+                                            identifier_types.insert(identifier.clone(), items[k].clone());
+                                        }
+                                        _ => {}
+                                    }
+                                    k += 1;
+                                }
+                            }
+                        }
+                    }
+                    _ => {}
+                }
+                i += 1;
+            }
 
             let result: Result<Type, String> = infer_expression_type(
                 program,
                 type_info,
-                &arg_types,
+                &identifier_types,
                 current_function,
                 &expression,
             );
