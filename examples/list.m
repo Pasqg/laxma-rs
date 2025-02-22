51c93ab7900bbbab247f7c4bc0bedc87a800ad62
Pasqg <pasquale.giovenale@gmail.com>
2025-02-22 10:56:55 +0100

    Add list related function implementations

--- examples/list.m
+++ examples/list.m
@@ -1,31 +1,74 @@
-type IntList -> Empty | List Int IntList
+type List [ 'T ] -> Empty | List 'T List [ 'T ]
 
-fn is_empty x : IntList =
+type Option [ 'T ] -> None | Some 'T
+
+type Pair [ 'P , 'Q ] -> Pair 'P 'Q
+
+fn empty -> List :: Empty ( )
+
+fn cons x : 'T xs : List [ 'T ] ->
+    List :: List ( x xs )
+
+fn empty? x : List [ 'T ] =
     Empty -> true
     List _ _ -> false
 
-fn empty ->
-    IntList :: Empty ( )
+fn concat xs : List [ 'T ] ys : List [ 'T ] =
+    _ , Empty -> xs
+    Empty , _ -> ys
+    List x xs , _ -> cons ( x concat ( xs ys ) )
 
-fn cons x : Int xs : IntList ->
-    IntList :: List ( x xs )
-
-fn list x : Int ->
+fn list x : 'T ->
     cons ( x empty ( ) )
 
-fn first x : IntList =
-    List fst _ -> fst
+fn first xs : List [ 'T ] =
+    List x _ -> x
 
-fn rest x : IntList =
+fn rest x : List [ 'T ] =
     Empty -> x
     List _ xs -> xs
 
-fn second x : IntList -> first ( rest ( x ) )
+fn second x : List [ 'T ] -> first ( rest ( x ) )
 
-fn length x : IntList = 
+fn length x : List [ 'T ] = 
     Empty -> 0
     List _ xs -> + ( 1 length ( xs ) )
 
-fn print_list a : IntList =
-    Empty -> print ( )
-    List x xs -> print ( x print_list ( xs ) )
+fn list_of n : Int x : 'T =
+    1 , _ -> list ( x )
+    _ , _ -> cons ( x list_of ( - ( n 1 ) x ) )
+
+fn filter pred : ( 'T ) -> Bool xs : List [ 'T ] =
+    _ , Empty -> empty ( )
+    _ , List x xs ->
+        if pred ( x )
+            cons ( x filter ( pred xs ) )
+            filter ( pred xs )
+
+fn map f : ( 'P ) -> 'Q xs : List [ 'P ] =
+    _ , Empty -> empty ( )
+    _ , List x xs -> cons ( f ( x ) map ( f xs ) )
+
+fn reduce
+        f : ( 'Result 'T ) -> 'Result
+        init : 'Result
+        xs : List [ 'T ]
+    =
+    _ , _ , Empty -> init
+    _ , _ , List x xs -> f ( reduce ( f init xs ) x )
+
+fn quicksort xs : List [ Int ] =
+    Empty -> empty ( )
+    List pivot xs ->
+        with smaller = ( x : Int ) -> < ( x pivot )
+            bigger = ( x : Int ) -> >= ( x pivot )
+            left = filter ( smaller xs )
+            right = filter ( bigger xs )
+            concat ( quicksort ( left ) concat ( list ( pivot ) quicksort ( right ) ) )
+
+fn random seed : Int -> with x = + ( seed * ( seed 1226138780960301465 ) ) if > ( x 0 ) x * ( -- ( 0 ) x )
+
+fn randomlist n : Int seed : Int =
+    0 , _ -> empty ( )
+    1 , _ -> list ( random ( seed ) )
+    _ , _ -> with xs = randomlist ( -- ( n ) seed ) cons ( random ( first ( xs ) ) xs )
